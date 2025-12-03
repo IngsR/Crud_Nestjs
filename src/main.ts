b750207fb873e71be53d10b8815750f7651eb83d
IngsR <ikhwan2ende@gmail.com>
2025-12-03 19:50:51 +0700

    adjust main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,9 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Enable global validation pipe
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
 
   // Enable CORS
   app.enableCors();
